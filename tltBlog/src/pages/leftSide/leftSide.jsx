858902993bd32498bac9f2d853b8bd2881aa4af4
VIT02 <VIT02@KSV.kenstone.com.tw>
2023-05-10 16:32:26 +0700

    Push from CPN

--- tltBlog/src/pages/leftSide/leftSide.jsx
+++ tltBlog/src/pages/leftSide/leftSide.jsx
@@ -1,23 +1,26 @@
 import myAvatar from "../../images/myAvatar.jpg"; // di chuyển ra bên ngoài đường dẫn cùng cấp ../../..n/
+
+import GruopClass from "./groupClass";
+
 function LeftSife () { 
     return (
         <>
             <div className="w-1/5 flex flex-col items-center bg-BgGrey py-11">
-                <div id="myProfile" className="font-PlayfairDisplay text-center flex flex-col items-center">
-                    <img src={myAvatar} alt="" className="w-44 h-44 rounded-full mx-16 mb-10"/>
-                    <h2 className="myProfile-name mb-3">TuiLaThang</h2>
-                    <p className="myProfile-slogan mb-6">Design Your Life</p>
+                <div id="myProfile" className="font-PlayfairDisplay text-center flex flex-col items-center border-b-2">
+                    <img src={myAvatar} alt="" className="w-36 h-36 rounded-full mx-16 mb-10"/>
+                    <h2 className="myProfile-name mb-3">TuilaThang</h2>
+                    <p className="myProfile-slogan mb-6 italic text-xs">"Design Your Life"</p>
                 </div>
-                <ul id="navBar" className="font-Qicksand text-xs text-textBlack text-center">
-                    <li className="py-2.5">HOME</li>
-                    <li className="py-2.5">ABOUT</li>
-                    <li className="py-2.5">SERVICES</li>
-                    <li className="py-2.5">SKILLLS</li>
-                    <li className="py-2.5">EDUCATION</li>
-                    <li className="py-2.5">EXPERIENCE</li>
-                    <li className="py-2.5">WORK</li>
-                    <li className="py-2.5">BLOG</li>
-                    <li className="py-2.5">CONTACT</li>
+                <ul id="navBar" className="font-Qicksand300 text-xs text-textBlack text-center cursor-pointer">
+                    <li id="home" className={`py-2.5 ${GruopClass.active} ${GruopClass.hover}`}><a>HOME</a></li>
+                    <li className={`py-2.5 ${GruopClass.hover}`}><a>ABOUT</a></li>
+                    <li className={`py-2.5 ${GruopClass.hover}`}><a>SERVICES</a></li>
+                    <li className={`py-2.5 ${GruopClass.hover}`}><a>SKILLLS</a></li>
+                    <li className={`py-2.5 ${GruopClass.hover}`}><a>EDUCATION</a></li>
+                    <li className={`py-2.5 ${GruopClass.hover}`}><a>EXPERIENCE</a></li>
+                    <li className={`py-2.5 ${GruopClass.hover}`}><a>WORK</a></li>
+                    <li className={`py-2.5 ${GruopClass.hover}`}><a>BLOG</a></li>
+                    <li className={`py-2.5 ${GruopClass.hover}`}><a>CONTACT</a></li>
                 </ul>
             </div>
         </>
