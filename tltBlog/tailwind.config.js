357579f2bd56c50646b2a08b5e3b068bd0cb9c7f
VIT02 <VIT02@KSV.kenstone.com.tw>
2023-05-11 15:28:22 +0700

    add screen reponsive plugin

--- tltBlog/tailwind.config.js
+++ tltBlog/tailwind.config.js
@@ -5,6 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      'sm': {'min': '576px', 'max': '767px'},
+      'md': {'min': '768px', 'max': '991px'},
+      'lg': {'min': '992px', 'max': '1199px'},
+      'xl': {'min': '1200px'},
+    },
     extend: {
       fontFamily : {
         Qicksand : ["Qicksand","arial","sans","selrif"] ,
