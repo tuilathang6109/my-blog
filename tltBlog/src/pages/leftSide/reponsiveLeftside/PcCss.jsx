ead894a0068319b02516a14d72dd4c3d1efb613a
VIT02 <VIT02@KSV.kenstone.com.tw>
2023-05-12 11:04:42 +0700

    ...

--- tltBlog/src/pages/leftSide/reponsiveLeftside/PcCss.jsx
+++ tltBlog/src/pages/leftSide/reponsiveLeftside/PcCss.jsx
@@ -1,7 +1,7 @@
 
 const PcCss = {
     hover:"hover:border-b-2 hover:scale-x-125 hover:scale-y-125 transition-all duration-300 ease-in-out",
-    leftSide : "flex flex-col h-screen items-center bg-BgGrey py-11",
+    leftSide : "flex flex-col  h-screen items-center bg-BgGrey py-11",
     profile : {
         profileMain : "font-PlayfairDisplay text-center flex flex-col items-center border-b-2",
         profileImg : "drop-shadow-2xl w-36 h-36 rounded-full mx-16 mb-6 border-solid border-2 border-blue-500 cursor-pointer" ,
