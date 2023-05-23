e987707d22cc723710f036fcf0fc9bc6891d76ce
VIT02 <VIT02@KSV.kenstone.com.tw>
2023-05-23 15:39:41 +0700

    home-1.1

--- tltBlog/tailwind.config.js
+++ tltBlog/tailwind.config.js
@@ -9,8 +9,8 @@ export default {
       's': {'max':'575px'},
       'sm': {'min': '576px', 'max': '767px'},
       'md': {'min': '768px', 'max': '991px'},
-      'lg': {'min': '992px', 'max': '1199px'},
-      'xl': {'min': '1200px'},
+      'lg': {'min': '992px', 'max': '1335px'},
+      'xl': {'min': '1336px', },
     },
     extend: {
       fontFamily : {
