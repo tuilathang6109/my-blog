d366a868b26df5267e837c66b170aba1d8f6bee1
VIT02 <VIT02@KSV.kenstone.com.tw>
2023-05-29 16:47:34 +0700

    conllapse-1

--- tltBlog/src/pages/content/contentComponent/education/Education.jsx
+++ tltBlog/src/pages/content/contentComponent/education/Education.jsx
@@ -1,7 +1,30 @@
+import React, { useState } from 'react';
 const Education = ()=>{
+    const [isExpanded, setIsExpanded] = useState(false);
+
+    const togglePanel = () => {
+      setIsExpanded(!isExpanded);
+    };
+
     return (
-        <div id="education" className="h-[500px]">
-        education
+        <div id="education" className="">
+            
+
+
+             <div className="panel">
+                <div className="panel-header" onClick={togglePanel}>
+                    <h3 className="panel-title px-[25px] py-[20px] bg-BgGrey">Tiêu đề</h3>
+                    <button className="panel-toggle">
+                    {isExpanded ? 'Thu gọn' : 'Mở rộng'}
+                    </button>
+                </div>
+                {isExpanded && (
+                    <div className="panel-body">
+                        Nội 
+                        dung
+                    </div>
+                )}
+                </div>
        </div>         
 
     )
