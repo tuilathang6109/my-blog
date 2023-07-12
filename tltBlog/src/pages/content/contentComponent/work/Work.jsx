0b22972f8185d93b45db4ffb66869e65566d2493
VIT02 <VIT02@KSV.kenstone.com.tw>
2023-07-12 13:48:29 +0700

    3333

--- tltBlog/src/pages/content/contentComponent/work/Work.jsx
+++ tltBlog/src/pages/content/contentComponent/work/Work.jsx
@@ -1,7 +1,11 @@
 const Works = ()=>{
     return (
-        <div id="work" className="h-[500px]">
-        Works
+        <div id="work" className="p-9 font-Kanit">
+             <div className="work-text text-left"> 
+                <div className=" text-[15px] opacity-50 mb-[15px] uppercase tracking-6">MY WORK</div>
+                <div className="text-[20px] font-bold font-PlayfairDisplay mb-[72px] tracking-6">RECENT WORK</div>
+            </div>
+                
        </div>         
 
     )
